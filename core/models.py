d5b794d52b61845432afa2bee1d23ab4d33a8b06
Lucas Pedreira Vital <2021105100039@aluno.ifro.edu.br>
2023-03-08 21:18:14 -0400

    wip

--- core/models.py
+++ core/models.py
@@ -31,3 +31,40 @@ class Servico(Base):
     def __str__(self):
         return self.nome
 
+
+class Cargo(Base):
+    cargo = models.CharField('Cargo', max_length=100)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self):
+        return self.cargo
+
+
+class Funcionario(Base):
+    nome = models.CharField('Nome', max_length=100)
+    bio = models.TextField('Biografia', max_length=255)
+    foto = StdImageField('Foto', upload_to='equipe',
+                           variations={
+                               'thumb': {
+                                   'width': 480,
+                                   'height': 480,
+                                   'crop': True
+                               }
+                           })
+    cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE)
+    facebook = models.CharField('Facebook', max_length=200)
+    twitter = models.CharField('Twitter', max_length=200)
+    instagram = models.CharField('Instagram', max_length=200)
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+
+    def __str__(self):
+        return self.nome
+
+
+
