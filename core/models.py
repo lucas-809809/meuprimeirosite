64369b5a067b5545985dbcd13018f5f4f7c3459e
Lucas Pedreira Vital <2021105100039@aluno.ifro.edu.br>
2023-03-08 20:37:11 -0400

    wip

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+
+class Base(models.Model):
+    criado = models.DateTimeField('Criado', auto_now_add=True)
+    modificao = models.DateTimeField('Atualizado', auto_now=True)
+    ativo = models.BooleanField('Ativo', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Servico(Base):
+    nome = models.CharField('Nome', max_length=100)
+    descricao = models.TextField('Descrição', max_length=500)
+    imagem = StdImageField('Imagem', upload_to='servico',
+                           variations={
+                               'thumb': {
+                                   'width': 480,
+                                   'height': 480,
+                                   'crop': True
+                               }
+                           }
+                           )
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self):
+        return self.nome
+
