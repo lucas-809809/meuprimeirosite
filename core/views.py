d6cea0220187aae23c59e2dd4eff2779cd94ee40
Lucas Pedreira Vital <2021105100039@aluno.ifro.edu.br>
2023-03-15 20:23:16 -0400

    'wip'

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from core.models import Funcionario
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data()
+        contexto['funcionarios'] = Funcionario.objects.all()
+        return contexto
+
