b3c616626640ad90b8d8964ce918cbd288ef9474
Lucas Pedreira Vital <2021105100039@aluno.ifro.edu.br>
2023-03-22 21:57:24 -0400

    wip

--- minhaempresa/settings.py
+++ minhaempresa/settings.py
@@ -131,6 +131,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGOUT_REDIRECT_URL = 'home'
+
 # configura e-mail para teste
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
